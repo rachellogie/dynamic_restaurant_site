8d846eb3a4e870cd36ef6536fde18d08684fc3c4
rachellogie <rachel.logie1@gmail.com>
2014-03-13 11:24:13 -0600

    allow copyright date to automatically change

--- spec/features/homepage_spec.rb
+++ spec/features/homepage_spec.rb
@@ -7,5 +7,13 @@ describe 'Visiting the home page' do
     expect(page).to have_content("Channa Masala")
     expect(page).to have_content("Chicken Tikka Masala")
   end
-  
+
+  it "displays the current year for the copyright" do
+    visit "/"
+
+    within("footer") do
+      expect(page).to have_content("2014")
+    end
+  end
+
 end
