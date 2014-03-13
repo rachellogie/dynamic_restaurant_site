cf7023d2a86c161c990d6f326c9aaf8c7a38bc90
rachellogie <rachel.logie1@gmail.com>
2014-03-13 15:00:19 -0600

    forgot to commit spec at same time

--- spec/features/homepage_spec.rb
+++ spec/features/homepage_spec.rb
@@ -7,5 +7,11 @@ describe 'Visiting the home page' do
     expect(page).to have_content("Channa Masala")
     expect(page).to have_content("Chicken Tikka Masala")
   end
+
+  it "displays the year dynamically" do
+    visit "/"
+
+    expect(page).to have_content("2014")
+  end
   
 end
